c494b1d1376fbaf5140e5d8ed0fae0f1bd3120cd
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-05-31 00:26:56 +0600

    added classes and main window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
+    JButton CourseBtn= new JButton("Courses");
+    JButton HelpBtn = new JButton("Help");
+    JButton AboutBtn= new JButton("About");
+
+    JPanel jp= new JPanel(new FlowLayout());
+    public Main(){
+        jp.add(CourseBtn);
+        jp.add(HelpBtn);
+        jp.add(AboutBtn);
+
+        JFrame frame= new JFrame("Desk");
+        frame.getContentPane().add(jp);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(960,540);
+
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new Main();
     }
 }
---
+++ src/Slides.java
@@ -0,0 +1,2 @@
+public class Slides {
+}
